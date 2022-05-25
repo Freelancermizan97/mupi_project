b1c4b8ca634e2032687e9b8a043fc6e2df189f4a
Freelancermizan97 <md.mizanhossan9990@gmail.com>
2022-05-25 16:02:53 +0600

    MY 1st CMNT

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -14,7 +14,7 @@ class _HomePageState extends State<HomePage> {
   final List<String> items1 = ['CMT', 'ENT', 'EEE', 'Civil', 'MC'];
   String? selectedItem1 = 'CMT';
 
-  final List<String> items1 = ['CMT', 'ENT', 'EEE', 'Civil', 'MC'];/////
+  final List<String> items1 = ['CMT', 'ENT', 'EEE', 'Civil', 'MC'];
   String? selectedItem1 = 'CMT';
 
   final List<String> items2 = ['CMT', 'ENT', 'EEE', 'Civil', 'MC'];
